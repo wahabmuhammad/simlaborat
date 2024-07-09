42a63c7cda0fd54f4103c1a21d12a1c6bd73dff0
Meko <69491257+Meko-repo@users.noreply.github.com>
2024-07-09 20:50:46 +0700

    adding table

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -20,7 +20,55 @@
             <a href="#" class="capitalize text-lg font-semibold">log out</a>
         </nav>
         <main class="flex-grow mx-8 mt-8">
-            
+            <table class="table-fixed w-full border-collapse">
+                <thead class="text-slate-800">
+                    <caption class="caption-top text-left text-4xl font-bold uppercase tracking-wider my-3">
+                        Daftar Pasien
+                    </caption>
+                    <tr class="h-16 text-xl capitalize text-slate-800 font-bold tracking-wider border-b-4 border-slate-800">
+                        <th class="w-16">no</th>
+                        <th class="">nama</th>
+                        <th class="w-72">tanggal</th>
+                        <th class="w-36">pemeriksaan</th>
+                        <th class="w-36">pembayaran</th>
+                        <th class="w-36">edit</th>
+                    </tr>
+                </thead>
+                <tbody class="text-xl">
+                    {{-- component breakpoint --}}
+                    <tr class="h-14 border-b-2 border-slate-300">
+                        <td class="text-center">1</td>
+                        <td class="items-baseline">
+                            <span class="block ml-10">Lorem ipsum dolor</span>
+                        </td>
+                        <td class="text-center">22/06/2024</td>
+                        <td class="text-center">selesai</td>
+                        <td class="text-center">selesai</td>
+                        <td class="text-center">Edit</td>
+                    </tr>
+                    {{-- end --}}
+                    <tr class="h-14 border-b-2 border-slate-300">
+                        <td class="text-center">2</td>
+                        <td class="items-baseline">
+                            <span class="block ml-10">Lorem ipsum dolor</span>
+                        </td>
+                        <td class="text-center">22/06/2024</td>
+                        <td class="text-center">selesai</td>
+                        <td class="text-center">selesai</td>
+                        <td class="text-center">Edit</td>
+                    </tr>
+                    <tr class="h-14 border-b-2 border-slate-300">
+                        <td class="text-center">3</td>
+                        <td class="items-baseline">
+                            <span class="block ml-10">Lorem ipsum dolor</span>
+                        </td>
+                        <td class="text-center">22/06/2024</td>
+                        <td class="text-center">selesai</td>
+                        <td class="text-center">selesai</td>
+                        <td class="text-center">Edit</td>
+                    </tr>
+                </tbody>
+              </table>
         </main>
 
     </body>
