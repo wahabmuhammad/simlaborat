f02d43d9bbdb14eb2b0a8ef1fca2ba63f9863d94
Meko <69491257+Meko-repo@users.noreply.github.com>
2024-07-17 19:11:15 +0700

    Fix Padding

--- resources/views/pendaftaran.blade.php
+++ resources/views/pendaftaran.blade.php
@@ -12,39 +12,38 @@
         <x-dashboard-navigation />
 
         <x-dashboard-content className="pt-20 h-fit w-screen lg:pt-0 lg:pl-64 lg:py-2">
-            <div class="lg:px-10 lg:pt-5">
-                <div class="p-4">
-                    <h1 class="text-gray-800 text-2xl font-bold tracking-wide capitalize">Pendaftaran Pasien</h1>
+            <div class="p-4">
+                <h1 class="text-gray-800 text-2xl font-bold tracking-wide capitalize">Pendaftaran Pasien</h1>
 
-                    <form action="POST" class="grid grid-cols-1 p-3 mt-2 gap-1 bg-gray-100 w-full rounded-md shadow-sm">
+                <form action="POST" class="grid grid-cols-1 p-3 mt-2 gap-1 bg-gray-100 w-full rounded-md shadow-sm">
+                    <div class="flex flex-col">
+                        <label for="nama-pasien" class="block min-w-9 font-medium flex-grow capitalize">Nama Pasien</label>
+                        <input type="text" name="" id="nama-pasien" class="mt-1 block border border-gray-400 rounded-sm px-1 outline-none focus:outline-2 focus:outline-gray-500">
+                    </div>
+                    <div class="grid grid-cols-2 gap-2">
                         <div class="flex flex-col">
-                            <label for="nama-pasien" class="block min-w-9 font-medium flex-grow capitalize">Nama Pasien</label>
+                            <label for="nama-pasien" class="block min-w-9 font-medium flex-grow capitalize">Tempat Lahir</label>
                             <input type="text" name="" id="nama-pasien" class="mt-1 block border border-gray-400 rounded-sm px-1 outline-none focus:outline-2 focus:outline-gray-500">
                         </div>
-                        <div class="grid grid-cols-2 gap-2">
-                            <div class="flex flex-col">
-                                <label for="nama-pasien" class="block min-w-9 font-medium flex-grow capitalize">Tempat Lahir</label>
-                                <input type="text" name="" id="nama-pasien" class="mt-1 block border border-gray-400 rounded-sm px-1 outline-none focus:outline-2 focus:outline-gray-500">
-                            </div>
-                            <div class="flex flex-col">
-                                <label for="nama-pasien" class="block min-w-9 font-medium flex-grow capitalize">tanggal lahir</label>
-                                <input type="date" name="" id="nama-pasien" class="mt-1 block border border-gray-400 rounded-sm px-1 outline-none focus:outline-2 focus:outline-gray-500">
-                            </div>
-                        </div>
                         <div class="flex flex-col">
-                            <label for="nama-pasien" class="block min-w-9 font-medium flex-grow capitalize">Alamat</label>
-                            <input type="text" name="" id="nama-pasien" class="mt-1 block border border-gray-400 rounded-sm px-1 outline-none focus:outline-2 focus:outline-gray-500">
+                            <label for="nama-pasien" class="block min-w-9 font-medium flex-grow capitalize">tanggal lahir</label>
+                            <input type="date" name="" id="nama-pasien" class="mt-1 block border border-gray-400 rounded-sm px-1 outline-none focus:outline-2 focus:outline-gray-500">
                         </div>
-                        <div class="mt-4">
-                            <button type="submit" class="bg-gray-800 text-gray-300 py-1 px-3 rounded-md font-medium tracking-wide">Submit</button>
-                        </div>
-                    </form>
-                </div>
-
-                <x-data-table title="Daftar Pasien">
-                    <x-data-lister />
-                </x-data-table>
+                    </div>
+                    <div class="flex flex-col">
+                        <label for="nama-pasien" class="block min-w-9 font-medium flex-grow capitalize">Alamat</label>
+                        <input type="text" name="" id="nama-pasien" class="mt-1 block border border-gray-400 rounded-sm px-1 outline-none focus:outline-2 focus:outline-gray-500">
+                    </div>
+                    <div class="mt-4">
+                        <button type="submit" class="bg-gray-800 text-gray-300 py-1 px-3 rounded-md font-medium tracking-wide">Submit</button>
+                    </div>
+                </form>
             </div>
+
+            <x-data-table title="Daftar Pasien">
+                <x-data-lister />
+            </x-data-table>
+
 
         </x-dashboard-content>
 
