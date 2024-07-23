1f1e56ff1890e927c34e5cecfe6653ad298c2ef4
Meko <69491257+Meko-repo@users.noreply.github.com>
2024-07-23 20:05:15 +0700

    Incomplete Update

--- resources/views/components/data-table.blade.php
+++ resources/views/components/data-table.blade.php
@@ -1,24 +1,25 @@
-<div>
-    <div>
-        <section class="p-4">
-            <h1 class="text-gray-800 text-2xl font-bold tracking-wide capitalize">{{$title}}</h1>
 
-            <div class="flex flex-wrap mt-2 gap-4">
-                <div class="py-1 px-2 bg-gray-100 rounded-md">
-                    <label for="tanggal" class="font-semibold text-gray-800">Tanggal</label>
-                    <input type="date" class="bg-gray-100 font-ligh w-fit"
-                        value="{{ Carbon\Carbon::Now()->format('Y-m-d') }}">
-                </div>
+<div class="md:p-5">
 
-                <div class="flex flex-wrap gap-0 w-fit content-center ">
-                    <button
-                        class="bg-gray-100 text-sm p-2 font-semibold text-gray-700 rounded-l-md hover:bg-gray-200 hover:text-gray-900">Belum</button>
-                    <button
-                        class="bg-gray-100 text-sm p-2 font-semibold text-gray-700 rounded-r-md hover:bg-gray-200 hover:text-gray-900">Sudah</button>
-                </div>
+    <section class="p-5">
+        <h1 class="text-gray-800 text-2xl font-bold tracking-wide capitalize">{{$title}}</h1>
+
+        <div class="flex flex-wrap mt-2 gap-4">
+            <div class="py-1 px-2 bg-gray-100 rounded-md shadow-md">
+                <label for="tanggal" class="font-semibold text-gray-800">Tanggal</label>
+                <input type="date" class="bg-gray-100 font-ligh w-fit"
+                    value="{{ Carbon\Carbon::Now()->format('Y-m-d') }}">
             </div>
-        </section>
-    </div>
+
+            <div class="flex flex-wrap gap-0 w-fit content-center shadow-md">
+                <button
+                    class="bg-gray-100 text-sm p-2 font-semibold text-gray-700 rounded-l-md hover:bg-gray-200 hover:text-gray-900">Belum</button>
+                <button
+                    class="bg-gray-100 text-sm p-2 font-semibold text-gray-700 rounded-r-md hover:bg-gray-200 hover:text-gray-900">Sudah</button>
+            </div>
+        </div>
+    </section>
 
     {{ $slot }}
+
 </div>
--- resources/views/pendaftaran.blade.php
+++ resources/views/pendaftaran.blade.php
@@ -13,7 +13,8 @@
 
         <x-dashboard-header title="Pendaftaran" />
 
-        <x-dashboard-content className="pt-20 h-fit w-screen lg:pt-0 lg:pl-64 lg:py-2">
+        <x-dashboard-content>
+
             <div class="p-4">
                 <h1 class="text-gray-800 text-2xl font-bold tracking-wide capitalize">Pendaftaran Pasien</h1>
 
@@ -22,37 +23,34 @@
                     method="POST">
                     @method('POST')
                     @csrf
+                <form action="POST" class="grid grid-cols-1 p-5 mt-2 gap-1 bg-gray-100 w-full rounded-md shadow-sm lg:px-6 lg:py-5">
                     <div class="flex flex-col">
-                        <label for="nama-pasien" class="block min-w-9 font-medium flex-grow capitalize">Nama
-                            Pasien</label>
-                        <input type="text" name="namaPasien" id="nama-pasien"
-                            class="mt-1 block border border-gray-400 rounded-sm px-1 outline-none focus:outline-2 focus:outline-gray-500">
+                        <label for="nama-pasien" class="block min-w-9 text-lg font-medium flex-grow capitalize lg:text-base">Nama Pasien</label>
+                        <input type="text" name="" id="nama-pasien" class="mt-1 py-1 block border border-gray-400 rounded-sm px-1  outline-none lg:py-0 focus:outline-2 focus:outline-gray-500">
                     </div>
                     <div class="grid grid-cols-2 gap-2">
                         <div class="flex flex-col">
-                            <label for="nama-pasien" class="block min-w-9 font-medium flex-grow capitalize">Tempat
-                                Lahir</label>
-                            <input type="text" name="tempatLahir" id="nama-pasien"
-                                class="mt-1 block border border-gray-400 rounded-sm px-1 outline-none focus:outline-2 focus:outline-gray-500">
+                            <label for="nama-pasien" class="block min-w-9 text-lg font-medium flex-grow capitalize lg:text-base">Tempat Lahir</label>
+                            <input type="text" name="" id="nama-pasien" class="mt-1 py-1 block border border-gray-400 rounded-sm px-1 outline-none lg:py-0 focus:outline-2 focus:outline-gray-500">
                         </div>
                         <div class="flex flex-col">
-                            <label for="nama-pasien" class="block min-w-9 font-medium flex-grow capitalize">tanggal
-                                lahir</label>
-                            <input type="date" name="tanggalLahir" id="nama-pasien"
-                                class="mt-1 block border border-gray-400 rounded-sm px-1 outline-none focus:outline-2 focus:outline-gray-500">
+                            <label for="nama-pasien" class="block min-w-9 text-lg font-medium flex-grow capitalize lg:text-base">tanggal lahir</label>
+                            <input type="date" name="" id="nama-pasien" class="mt-1 py-1 block border border-gray-400 rounded-sm px-1 outline-none lg:py-0 focus:outline-2 focus:outline-gray-500">
                         </div>
                     </div>
                     <div class="flex flex-col">
-                        <label for="nama-pasien" class="block min-w-9 font-medium flex-grow capitalize">Alamat</label>
-                        <input type="text" name="alamat" id="nama-pasien"
-                            class="mt-1 block border border-gray-400 rounded-sm px-1 outline-none focus:outline-2 focus:outline-gray-500">
+                        <label for="nama-pasien" class="block min-w-9 text-lg font-medium flex-grow capitalize lg:text-base">Alamat</label>
+                        <input type="text" name="" id="nama-pasien" class="mt-1 py-1 block border border-gray-400 rounded-sm px-1 outline-none lg:py-0 focus:outline-2 focus:outline-gray-500">
                     </div>
                     <div class="mt-4">
-                        <button type="submit"
-                            class="bg-gray-800 text-gray-300 py-1 px-3 rounded-md font-medium tracking-wide">Submit</button>
+                        <button type="submit" class="bg-gray-800 text-gray-300 py-2 px-3 rounded-md font-medium tracking-wide lg:py-1">Submit</button>
                     </div>
                 </form>
             </div>
+
+            @php
+                gettype()
+            @endphp
 
             <x-data-table title="Daftar Pasien">
                 <x-data-lister />
